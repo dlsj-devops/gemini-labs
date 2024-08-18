fdf202862e923ae27bb5bee8d03cba1a3dc28461
Daniel de Lima Silva Júnior <dlima@tre-pa.jus.br>
2024-08-18 16:33:48 -0300

    formatando prompt estruturado em categorias

--- alura-gemini-node-js-integrando-aplicacao-api-google/index.js
+++ alura-gemini-node-js-integrando-aplicacao-api-google/index.js
@@ -8,12 +8,20 @@ async function run() {
   // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-  let prompt = "Você é um site de viagens e deve responder somente sobre esse assunto" +
-  " Caso o usuário pergunte sobre algo diferente, diga que não pode responder. " +
-  " O usuário escolheu: ";
-  prompt += await fazerPergunta("Me fale sobre o destino que deseja conhecer: ");
+  const prompt = await fazerPergunta("Me fale sobre o destino que deseja conhecer: ");
 
-  const result = await model.generateContent(prompt);
+  const parts = [
+    {text: "Você é o chatbot de um site que vende pacotes de viagem. Ao ser perguntado sobre algum destino, como bairro, cidade, estado, país, continente e pontos turísticos diversos, você poderá fornecer informações. Caso seja perguntado sobre algo que não ter relação com viagem e turismo, informe que não poder responder a essa dúvida.\n\nPara formular a resposta, quero que os tópicos apareçam como lista com marcadores e sempre deve conter as categorias: características, localização, cultura, pontos turísticos e culinária."},
+    {text: "input: me fale sobre o bairro do flamengo, no Rio de Janeiro"},
+    {text: "output: **Bairro do Flamengo, Rio de Janeiro**\n\n* **Características:**\n    * Bairro nobre e residencial\n    * Conhecido por sua orla e parques\n    * Coração financeiro da cidade\n* **Localização:**\n    * Zona Sul do Rio de Janeiro\n    * Limita-se com as praias de Copacabana, Botafogo e Glória\n* **Cultura:**\n    * Sede do Museu de Arte Moderna (MAM) e da Marina da Glória\n    * Abriga o Parque do Flamengo, um dos maiores parques urbanos do país\n* **Pontos Turísticos:**\n    * Orla do Flamengo (anel viário com ciclovia e vista panorâmica)\n    * Museu de Arte Moderna (MAM)\n    * Marina da Glória (e palco do Reveillon)\n    * Parque do Flamengo\n    * Morro da Viúva (com vista para o Pão de Açúcar)\n* **Culinária:**\n    * Diversos restaurantes e bares na orla e no Parque do Flamengo\n    * Opções gastronômicas variadas, de frutos do mar a comida internacional"},
+    {text: `input: me fale sobre o destino ${prompt}`},
+    {text: "output: "},
+  ];
+    
+  const result = await model.generateContent(
+    {contents: [{ role: "user", parts }]}
+  );
+  
   const response = await result.response;
   const text = response.text();
   console.log(text);
